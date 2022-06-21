f8ebaf4a4cc770a3c5099a2db355903569c035e4
sibowen535 <bowen.simon@gmail.com>
2022-06-21 16:27:58 +0100

    post route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ import {query} from "../db/index.js"
 
 const router = Express.Router();
 
-router.post('/', () => {
-    
+router.post('/', (req, res) => {
+    const resourceObj = req.body;
+    let res = await query(
+        `INSERT INTO resources
+        (title, url, content_type, topic)
+        VALUES ($1, $2, $3, $4);`,
+    [resourceObj.title, resourceObj.url, resourceObj.content_type, resourceObj.topic]
+    );
 });
