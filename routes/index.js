ca05369ff883e53e339207f003980cbb9ccbf50f
Lorentz Bloom <lorentzbloomguitar@gmail.com>
2022-06-21 16:17:17 +0100

    add get req handler to routes-index.js

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+router.get('/', async (req, res) {
+  const result = await getResources();
+  res.json({
+    success: true,
+    payload: result,
+  })
+})
+
 export default router;
