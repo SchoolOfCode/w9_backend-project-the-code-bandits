69e6c7bd791795e62cde0b72fd64dd7ffd2edf9c
piratejas <piratejas@gmail.com>
2022-06-21 16:23:42 +0100

    added getResources function and exported from models/imdex.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 import express from "express";
+import { getResources } from "../models/resources";
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const result = await getResources();
   res.json({
     success: true,
     payload: result,
-  })
-})
+  });
+});
 
 export default router;
