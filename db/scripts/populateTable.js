5a49ee43bbbc5d27e2cb08e5beac4e07bbc1a17d
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-06-21 15:00:01 +0100

    added error catcher to populateTable script

--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -10,18 +10,22 @@ const testResource = [
 ];
 
 async function populateTable() {
-  for (let i = 0; i < testResource.length; i++) {
-    const res = await query(
-      `INSERT INTO resources
-      ( title, url, content_type, topic)
-      VALUES ($1, $2, $3, $4);`,
-      [
-        testResource[i].title,
-        testResource[i].url,
-        testResource[i].content_type,
-        testResource[i].topic,
-      ]
-    );
+  try {
+    for (let i = 0; i < testResource.length; i++) {
+      const res = await query(
+        `INSERT INTO resources
+          ( title, url, content_type, topic)
+          VALUES ($1, $2, $3, $4);`,
+        [
+          testResource[i].title,
+          testResource[i].url,
+          testResource[i].content_type,
+          testResource[i].topic,
+        ]
+      );
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
 
